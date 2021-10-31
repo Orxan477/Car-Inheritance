c941a71a31bd712079888b47b721ce7794ea5b61
Orxan Qanbarov <41814449+Orxan477@users.noreply.github.com>
2021-10-31 20:36:46 +0400

    Iterhence Fix

--- Car-Interhence/Program.cs
+++ Car-Interhence/Program.cs
@@ -6,48 +6,48 @@ namespace Car_Interhence
     {
         static void Main(string[] args)
         {
-            Car car1 = new Car("Toyota", "Camry");
-            car1.InfoCar();
+            //Car car1 = new Car("Toyota", "Camry");
+            //car1.InfoCar();
 
-            Console.WriteLine("-----------------------");
+            //Console.WriteLine("-----------------------");
 
-            Bus bus1 = new Bus("Isuzu", "Turkuaz");
-            bus1.InfoBus();
+            //Bus bus1 = new Bus("Isuzu", "Turkuaz");
+            //bus1.InfoBus();
 
-            Console.WriteLine("-----------------------");
+            //Console.WriteLine("-----------------------");
 
-            Truck truck1 = new Truck("Shacman", "F3000",1990,250000,"Black","Manual",25,1000);
-            truck1.InfoTruck();
+            //Truck truck1 = new Truck("Shacman", "F3000",1990,250000,"Black","Manual",25,1000);
+            //truck1.InfoTruck();
 
-            Console.WriteLine("-----------------------");
+            //Console.WriteLine("-----------------------");
 
-            Motorbike moto1 = new Motorbike("SYM", "X-PRO", 2019, 0, "White", true);
-            moto1.MotoInfo();
+            //Motorbike moto1 = new Motorbike("SYM", "X-PRO", 2019, 0, "White", true);
+            //moto1.MotoInfo();
 
-            Console.WriteLine("-----------------------");
+            //Console.WriteLine("-----------------------");
 
-            Velobike velo1 = new Velobike("Brave", 2015, 120000, "Blue", false, "Normal");
-            velo1.VeloInfo();
+            //Velobike velo1 = new Velobike("Brave", 2015, 120000, "Blue", false, "Normal");
+            //velo1.VeloInfo();
 
-            Console.WriteLine("-----------------------");
+            //Console.WriteLine("-----------------------");
 
-            Plane plane1 = new Plane("Azal", "AirBus", 1988, "Blue", 100);
-            plane1.PlaneInfo();
+            //Plane plane1 = new Plane("Azal", "AirBus", 1988, "Blue", 100);
+            //plane1.PlaneInfo();
 
-            Console.WriteLine("-----------------------");
+            //Console.WriteLine("-----------------------");
 
-            Helicopter helicopter1 = new Helicopter("Azal", "AirBus", 1988, "Blue", 100);
-            helicopter1.HelicopterInfo();
+            //Helicopter helicopter1 = new Helicopter("Azal", "AirBus", 1988, "Blue", 100);
+            //helicopter1.HelicopterInfo();
 
-            Console.WriteLine("-----------------------");
+            //Console.WriteLine("-----------------------");
 
-            Boat boat1 = new Boat("Yamaha", "Qayiq");
-            boat1.BoatInfo();
+            //Boat boat1 = new Boat("Yamaha", "Qayiq");
+            //boat1.BoatInfo();
 
-            Console.WriteLine("-----------------------");
+            //Console.WriteLine("-----------------------");
 
-            Sheep sheep1 = new Sheep("Yamaha", "Gemi");
-            sheep1.SheepInfo();
+            //Sheep sheep1 = new Sheep("Yamaha", "Gemi");
+            //sheep1.SheepInfo();
         }
     }
 }
